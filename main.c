1dac7c5b4bd52cc28ece1a25de830bd8510056ed
ravensramen <153202736+ravensramen@users.noreply.github.com>
2024-03-14 18:20:29 -0700

    main.c
    final update

--- main.c
+++ main.c
@@ -1,27 +1,35 @@
+//Name: Sydnee Boothby
+//Class: CptS 121
+//Assignment: PA5
+//Due Date: 03/20
+//Instructor: Andy O'Fallon
+//TA: Claire Monsoon
+//Description: This program includes all the neccessary functions to play a game of yahtzee with two players.
+//It includes the use of various used-based functions, loops, pointers, and arrays learned thus far in CptS 121. 
+
 #include "header.h"
 
 int main(void) {
-	srand((time(NULL)));
+	srand((time(NULL))); //allows for random dice rolls
 
-	int user_choice = 0;
+	int user_choice = 0; //initialize variable for user menu choice
 
 	printf("Welcome to the Yahtzee!\n\n");
 	do {
 
-		user_choice = determine_user_choice();
+		user_choice = determine_user_choice(); //stores user choice
 
-		switch (user_choice) {
+		switch (user_choice) { 
 		case 1: display_rules();
 			break;
 		case 2:
 			yahtzee_gameplay();
 			break;
 		case 3:
-			printf("goodbye");
+			printf("\nGoodbye, thanks for playing :)\n");
 			return 0;
 		}
-		//system('cls')
-	} while (user_choice != 3);
+	} while (user_choice != 3); //repeats menu until user chooses to leave program
 
-	return 0;
+	return 0; //indicates program ran successfully
 }
